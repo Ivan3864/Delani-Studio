23ba59c01d05d2544ae4fcbf26cb0ee206ad6e52
Ivan 3864 <kiet3864@gmail.com>
2020-10-12 19:48:07 +0300

    addition of java script commands

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#1").click(function() {
+    $(".column1wwd").toggle();
+    $(".column1text").fadeToggle();
+  });
+});
+$(document).ready(function() {
+  $("#2").click(function() {
+    $(".column2wwd").toggle();
+    $(".column2text").fadeToggle();
+  });
+});
+$(document).ready(function() {
+  $("#3").click(function() {
+    $(".column3wwd").toggle();
+    $(".column3text").fadeToggle();
+  });
+});
